3bc747ff0d5a395929fd9d0c60d4689a9269b347
Fernando Butzke <nandoproject209@gmail.com>
2021-04-24 16:18:41 -0300

    Adding components

--- reactjs/01-github-explorer/src/components/RepositoryList.jsx
+++ reactjs/01-github-explorer/src/components/RepositoryList.jsx
@@ -1,10 +1,19 @@
 import { RepositoryItem } from "./RepositoryItem";
 
+const repository = {
+    name: "unform",
+    description: "Forms in React",
+    link: "https://github.com/unform/unform"
+}
+
 export function RepositoryList() {
     return (
         <section className="repository-list">
             <h1>Repository List - nandobutzke</h1>
             <ul>
+                <RepositoryItem repository={repository} />
+                <RepositoryItem />
+                <RepositoryItem />
                 <RepositoryItem />
             </ul>
         </section>
