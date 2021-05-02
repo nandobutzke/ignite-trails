3c7fabe39a1b36a617fdd05284516379deb4b8aa
Fernando Butzke <nandoproject209@gmail.com>
2021-05-02 13:49:12 -0300

    Adding TypeScript

--- reactjs/01-github-explorer/src/components/RepositoryList.tsx
+++ reactjs/01-github-explorer/src/components/RepositoryList.tsx
@@ -3,8 +3,14 @@ import { useEffect, useState } from "react";
 
 import '../styles/repositories.scss'
 
+interface Repository {
+    name: string
+    description: string;
+    html_url: string;
+}
+
 export function RepositoryList() {
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<Repository[]>([]);
 
     useEffect(() => {
         fetch('https://api.github.com/users/nandobutzke/repos')
@@ -18,7 +24,7 @@ export function RepositoryList() {
 
             <ul>
                 {repositories.map(repository => {
-                    return <RepositoryItem key={repository.node_id} repository={repository} />
+                    return <RepositoryItem key={repository.name} repository={repository}  />
                 })}
             </ul>
         </section>
