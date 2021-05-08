e08982dbfed87619eb509ddd8336e0c8b94e133a
Fernando Butzke <nandoproject209@gmail.com>
2021-05-07 22:44:32 -0300

    Challenge partially finished!

--- challenges-reactjs/challenge-01-react-concepts/src/components/TaskList.tsx
+++ challenges-reactjs/challenge-01-react-concepts/src/components/TaskList.tsx
@@ -13,17 +13,49 @@ interface Task {
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
-
+  //My resolve: const [boolValue, setBoolValue] = useState(Boolean);
+  let randomValue = Math.random();
+  
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    const task = {
+      id: randomValue,
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    if(task.title) {
+      setTasks([...tasks, task]);
+    } 
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const tasksCompleted = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+    
+    setTasks(tasksCompleted);
+
+    /*
+    * My resolve
+    * tasks.find(task => {
+    * if(task.id === id) {
+    *   setBoolValue(true)
+    *  }
+    * });
+    */
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    /*
+    * My resolve
+    * tasks.splice(id, 1)
+    * setTasks([...tasks])
+    */
+
+    const filtertedTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(filtertedTasks)
   }
 
   return (
