8ff9fc7301326547ef9a32818715c7bced1191ce
Fernando Butzke <nandoproject209@gmail.com>
2021-07-03 11:33:01 -0300

    Adding handleCreateNewRepository

--- reactjs/01-github-explorer/src/components/RepositoryList.tsx
+++ reactjs/01-github-explorer/src/components/RepositoryList.tsx
@@ -1,5 +1,5 @@
 import { RepositoryItem } from "./RepositoryItem";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 
 import '../styles/repositories.scss'
 
@@ -11,20 +11,64 @@ interface Repository {
 
 export function RepositoryList() {
     const [repositories, setRepositories] = useState<Repository[]>([]);
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [html_url, setHtmlUrl] = useState('');
 
     useEffect(() => {
         fetch('https://api.github.com/users/nandobutzke/repos')
         .then(response => response.json())
         .then(data => setRepositories(data))
-    }, [])
+    }, []);
+
+    function handleCreateNewRepository(event: FormEvent) {
+        event.preventDefault();
+
+        const repository = {
+            name: name,
+            description: description,
+            html_url: html_url
+        };
+
+        setRepositories([repository, ...repositories]);
+        setName('');
+        setDescription('');
+        setHtmlUrl('');
+    }
 
     return (
         <section className="repository-list">
-            <h1>Repository List - nandobutzke</h1>
+            <h1>Repository List</h1>
+
+            <form onSubmit={handleCreateNewRepository}>
+                <input 
+                    type="text"
+                    placeholder="Nome"
+                    value={name}
+                    onChange={event => setName(event.target.value)}
+                />
+                <input 
+                    type="text"
+                    placeholder="Descrição"
+                    value={description}
+                    onChange={event => setDescription(event.target.value)}
+                />
+                <input 
+                    type="text"
+                    placeholder="URL"
+                    value={html_url}
+                    onChange={event => setHtmlUrl(event.target.value)}
+                />
+                <button
+                    type="submit"
+                >
+                    Criar repositório
+                </button>
+            </form>
 
             <ul>
                 {repositories.map(repository => {
-                    return <RepositoryItem key={repository.name} repository={repository}  />
+                    return <RepositoryItem key={repository.name} repository={repository} />;
                 })}
             </ul>
         </section>
