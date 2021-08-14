95ee7a10b5f1406f8f41bfaaa221ebfb421e83d0
Fernando Butzke <nandoproject209@gmail.com>
2021-08-13 23:45:41 -0300

    Configuring Stripe + SSG implementation

--- reactjs/03-ignews/src/pages/index.tsx
+++ reactjs/03-ignews/src/pages/index.tsx
@@ -1,10 +1,21 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+
+import { stripe } from '../services/stripe';
+
 import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -13,16 +24,37 @@ export default function Home() {
 
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
-          <span>👋 Hey, welcome</span>
+          <span>👋 Hey, welcome!</span>
           <h1>News about <br />the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9,90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <Image src="/images/avatar.svg" alt="Girl coding" height={500} width={500} />
       </main>
     </>
   )
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JNTE1EY5OrXpo91KrtG0fZj', {
+    expand: ['product']
+  });
+
+  const product = { 
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100)
+  }
+
+  return { 
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24, //24 hours
+  }  
+} 
