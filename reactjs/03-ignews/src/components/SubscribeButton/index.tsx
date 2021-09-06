d811d22fdf3f12bc733fe1711d7db88265109fbe
Fernando Butzke <nandoproject209@gmail.com>
2021-09-06 13:01:54 -0300

    Validating active signature

--- reactjs/03-ignews/src/components/SubscribeButton/index.tsx
+++ reactjs/03-ignews/src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from 'next-auth/client';
+import router, { useRouter } from 'next/router';
+
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
@@ -13,6 +15,11 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
     async function handleSubscribe() {
         if(!session) {
             signIn('github');
+            return;
+        }
+
+        if(session.activeSubscription) {
+            router.push('/posts');
             return;
         }
 
